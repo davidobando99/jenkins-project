f0146f7c52319e3378f3ce0c2dece346c181bf98
David Obando <david.obando2@nordstrom.com>
2022-09-20 15:43:39 -0500

    Revert "Add biginteger instead integer"
    This reverts commit fe1a64131eceed9b281411e11b57563af2daf48f.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,11 @@
 package org.example;
 
-import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         char operator;
-        BigInteger number1, number2, result;
+        Integer number1, number2, result;
 
         // create an object of Scanner class
         Scanner input = new Scanner(System.in);
@@ -17,34 +16,34 @@ public class Main {
 
         // ask users to enter numbers
         System.out.println("Enter first number");
-        number1 = input.nextBigInteger();
+        number1 = input.nextInt();
 
         System.out.println("Enter second number");
-        number2 = input.nextBigInteger();
+        number2 = input.nextInt();
 
         switch (operator) {
 
             // performs addition between numbers
             case '+':
-                result = number1.add(number2);
+                result = number1 + number2;
                 System.out.println(number1 + " + " + number2 + " = " + result);
                 break;
 
             // performs subtraction between numbers
             case '-':
-                result = number1.subtract(number2);
+                result = number1 - number2;
                 System.out.println(number1 + " - " + number2 + " = " + result);
                 break;
 
             // performs multiplication between numbers
             case '*':
-                result = number1.multiply(number2);
+                result = number1 * number2;
                 System.out.println(number1 + " * " + number2 + " = " + result);
                 break;
 
             // performs division between numbers
             case '/':
-                result = number1.divide(number2);
+                result = number1 / number2;
                 System.out.println(number1 + " / " + number2 + " = " + result);
                 break;
 
