c1760026e36a8a12c621a7e2f24daecec30930fc
David Obando <david.obando2@nordstrom.com>
2022-09-20 15:46:04 -0500

    Add biginteger instead integer

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,12 @@
 package org.example;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         char operator;
-        Integer number1, number2, result;
+        BigInteger number1, number2, result;
 
         // create an object of Scanner class
         Scanner input = new Scanner(System.in);
@@ -16,34 +17,34 @@ public class Main {
 
         // ask users to enter numbers
         System.out.println("Enter first number");
-        number1 = input.nextInt();
+        number1 = input.nextBigInteger();
 
         System.out.println("Enter second number");
-        number2 = input.nextInt();
+        number2 = input.nextBigInteger();
 
         switch (operator) {
 
             // performs addition between numbers
             case '+':
-                result = number1 + number2;
+                result = number1.add(number2);
                 System.out.println(number1 + " + " + number2 + " = " + result);
                 break;
 
             // performs subtraction between numbers
             case '-':
-                result = number1 - number2;
+                result = number1.subtract(number2);
                 System.out.println(number1 + " - " + number2 + " = " + result);
                 break;
 
             // performs multiplication between numbers
             case '*':
-                result = number1 * number2;
+                result = number1.multiply(number2);
                 System.out.println(number1 + " * " + number2 + " = " + result);
                 break;
 
             // performs division between numbers
             case '/':
-                result = number1 / number2;
+                result = number1.divide(number2);
                 System.out.println(number1 + " / " + number2 + " = " + result);
                 break;
 
